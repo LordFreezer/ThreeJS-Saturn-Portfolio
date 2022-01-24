d900de068c6defbd71daf9f434d238b7bdf989c6
cmarshall19 <cmarshall19@uco.edu>
2022-01-23 18:47:48 -0600

    updated import path for three

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// old
 //import './style.css';
 //import * as THREE from 'three';
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as THREE from 'https://cdn.skypack.dev/three';
-import { OrbitControls } from 'https://rawgit.com/mrdoob/three.js/dev/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './node_modules/three/build/three.module';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// maybe broken
+//import * as THREE from 'https://cdn.skypack.dev/three';
+//import { OrbitControls } from 'https://rawgit.com/mrdoob/three.js/dev/examples/jsm/controls/OrbitControls.js';
+
+// maybe new
+//import * as THREE from 'https://threejs.org/build/three.js';
+
+//import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
+//import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports/optimized/three.js';
+//import * as THREE from 'https://unpkg.com/three@0.120.1/build/three.module.js';
+//import { OrbitControls } from 'https://threejs.org/examples/js/controls/OrbitControls.js';
 
 // scene
 const scene = new THREE.Scene();
