a34a77917f8775ce4d91e66a9424e91b8a268f96
cmarshall19 <cmarshall19@uco.edu>
2022-01-23 18:58:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // old
 //import './style.css';
 //import * as THREE from 'three';
-import * as THREE from './node_modules/three/build/three.module';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
+
+import * as THREE from '/node_modules/three/build/three.module';
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls';
+//import * as THREE from './node_modules/three/build/three.module';
+//import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
 
 // maybe broken
 //import * as THREE from 'https://cdn.skypack.dev/three';
